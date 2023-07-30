f206d347963965fe1ee3e0a21e1360b8d9fa325a
Umaid Shaikh <Umaid.Shaikh@asl.mum.sst>
2023-07-30 21:41:29 +0530

    TicTacToe

---
+++ TicTacToe/user.js
@@ -0,0 +1,10 @@
+const Game = require("./Game");
+
+let g1=Game.newGame("Umaid","Siddhi");
+let g2=Game.newGame("Sahil","Amisha");
+
+console.log(g1.play(0))
+console.log(g1.play(3))
+console.log(g1.play(1))
+console.log(g1.play(4))
+console.log(g1.play(2))
